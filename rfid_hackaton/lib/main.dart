30acffb9e174f1842b90ddaf8c442d1da98611d8
elblogbruno <elblogdebruno@outlook.es>
2022-05-02 18:03:10 +0200

    feat: route form, with marker creation and good design

--- rfid_hackaton/lib/main.dart
+++ rfid_hackaton/lib/main.dart
@@ -25,9 +25,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home:
-      // new HomeScreen(),
-      const MyHomePage(title: 'Mi App'),
+      darkTheme: ThemeData.dark(),
+      home: const MyHomePage(title: 'RFID Bus Tracker'),
     );
   }
 }
@@ -78,84 +77,41 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: const MapView(title: 'New Route',),
+      drawer: buildDrawer(context),
+    );
+  }
+
+  Widget buildDrawer(BuildContext context) {
+    return Drawer(
+        child: ListView(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const UserAccountsDrawerHeader(
+              accountName: Text("Prueba Prueba"),
+              accountEmail: Text("email@prueba.es"),
+              currentAccountPicture: CircleAvatar(backgroundColor: Colors.white),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            /* Button that when clicked opens map view */
-            RaisedButton(
-              child: Text('Open Map View'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MapView(title: 'Choose your destination',)),
-                );
+            ListTile(
+              title: Text("Home"),
+              leading: Icon(Icons.home),
+              onTap: (){
+                Navigator.pop(context);
               },
             ),
+            ListTile(
+              title: Text("Profile"),
+              leading: Icon(Icons.account_circle),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("Version App 0.0.1"),
+              onTap: (){},
+            ),
           ],
-        ),
-      ),
-      drawer: Drawer(
-          child: new ListView(
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text("Prueba Prueba"),
-                accountEmail: Text("email@prueba.es"),
-                currentAccountPicture: CircleAvatar(backgroundColor: Colors.white),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text("Home"),
-                leading: Icon(Icons.home),
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MapView(title: 'Choose your destination',)),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text("Profile"),
-                leading: Icon(Icons.account_circle),
-                onTap: (){},
-              ),
-              ListTile(
-                title: Text("Version App 0.0.1"),
-                onTap: (){},
-              ),
-            ],
-          )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
     );
   }
 }
--- rfid_hackaton/lib/views/map_view.dart
+++ rfid_hackaton/lib/views/map_view.dart
@@ -1,19 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MapView extends StatefulWidget {
   const MapView({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -24,64 +17,174 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   final Completer<GoogleMapController> _controller = Completer();
 
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+  final PanelController _pc = PanelController();
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  Map<PolylineId, Polyline> polylines = <PolylineId, Polyline>{};
+  int _polylineIdCounter = 0;
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  List<LatLng> polylineCoordinates = [];
+
+  void zoomToPolyline() {
+    const double polylineWidth = 10;
+
+    const double edgePadding = polylineWidth * .15;
+
+    LatLngBounds bounds = LatLngBounds(
+      southwest: polylineCoordinates.first,
+      northeast: polylineCoordinates.last
+    );
+
+    CameraUpdate cu = CameraUpdate.newLatLngBounds(bounds, edgePadding);
+
+    _controller.future.then((GoogleMapController controller) {
+      controller.animateCamera(cu);
+    });
   }
+
+  void createPolyLine(List<LatLng> points) {
+    final int polylineCount = polylines.length;
+
+    if (polylineCount == 2) {
+      return;
+    }
+
+    final String polylineIdVal = 'polyline_id_$_polylineIdCounter';
+
+    _polylineIdCounter++;
+
+    final PolylineId polylineId = PolylineId(polylineIdVal);
+
+    final Polyline polyline = Polyline(
+      polylineId: polylineId,
+      consumeTapEvents: true,
+      color: Colors.orange,
+      width: 5,
+      points: points,
+    );
+
+    if (!polylines.containsKey(polylineId)) {
+      setState(() {
+        polylines[polylineId] = polyline;
+      });
+    }
+  }
+
+  /// This generates a marker for the map.
+  void generateMarker(String address, String title, double lat, double lng) {
+    final MarkerId markerId = MarkerId(address);
+
+    // creating a new MARKER
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(lat, lng),
+      infoWindow: InfoWindow(title: title, snippet: '*'),
+      onTap: () {
+        print('Marker Tapped');
+      },
+    );
+
+    setState(() {
+      // adding a new marker to map
+      markers[markerId] = marker;
+    });
+  }
+
+  /// It hides the panel for some seconds so the user can see the map
+  /// and then it shows the panel again
+
+  Future<void> showMarkerAnimation(GoogleMapController controller, String query) async {
+    _pc.close();
+
+    // hide keyboard
+    FocusScope.of(context).requestFocus(FocusNode());
+
+    // wait for 1 second
+    await Future.delayed(const Duration(seconds: 3));
+    // then move to the marker
+    LatLng latLng = markers[MarkerId(query)]!.position;
+
+    LatLng  offsetLoc = LatLng(latLng.latitude - 0.010, latLng.longitude);
+
+    controller.animateCamera(CameraUpdate.newLatLngZoom(offsetLoc, 14.4746));
+    // wait for 1 second
+    _pc.open();
+  }
+
+  /// This function is called when the user writes a query.
+  /// It will call the Google Maps API to get the coordinates of the query.
+  /// It will then call the generateMarker function to add a marker to the map.
+  /// Finally, it will call the showMarkerAnimation function to animate the camera to the marker.
+  /// This function is called when the user writes a query.
+  Future<void> moveToPossibleLatLong(String query) async {
+    final GoogleMapController controller = await _controller.future;
+
+    try {
+      List<Location> locations = await locationFromAddress(query);
+      Location location = locations.first;
+
+      polylineCoordinates.add(LatLng(location.latitude, location.longitude));
+
+      generateMarker(query, query, location.latitude, location.longitude);
+
+      final cameraUpdate = CameraUpdate.newLatLngZoom(LatLng(location.latitude, location.longitude), 14);
+
+      controller.animateCamera(cameraUpdate).then((value) =>
+        showMarkerAnimation(controller, query));
+
+    } catch (e) {
+      showAlertDialog(context,  'No location found for $query');
+    }
+  }
+
+  String _location = "";
+  String _destination = "";
+
   double circularRadius = 10;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },
-            onTap: (LatLng latLng) {
-              showAlertDialog(context, latLng.toString());
+    BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(circularRadius),
+      topRight: Radius.circular(circularRadius),
+    );
 
-              /*setState(() {
-                showAlertDialog(context, latLng.toString());
-              });*/
-            },
-          ),
-          Align(
-              alignment: Alignment.bottomRight,
-              child: buildBottomSheet(),
-          ),
-        ],
+    return Scaffold(
+      body: SlidingUpPanel(
+        renderPanelSheet: false,
+        collapsed: buildCollapsed(radius, isExpanded: false),
+        panel: buildBottomSheet(radius),
+        body: buildGoogleMaps(context),
+        borderRadius:  radius,
+        controller: _pc,
+          maxHeight: MediaQuery.of(context).size.height * 0.55,
+        onPanelSlide: (double pos) {
+          setState(() {
+            circularRadius = 10 + (pos * 10);
+          });
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _goToTheLake,
-        tooltip: 'Next',
-        child: const Icon(Icons.navigate_next),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  /// Builds the google maps widget
+  Widget buildGoogleMaps(BuildContext context) {
+    return GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(markers.values),
+        polylines: Set<Polyline>.of(polylines.values),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        onTap: (LatLng latLng) {
+          showAlertDialog(context, latLng.toString());
+        },
     );
   }
 
@@ -112,90 +215,154 @@ class _MapViewState extends State<MapView> {
   }
 
 
-  Widget buildBottomSheet() {
+  Widget buildCollapsed(BorderRadiusGeometry radius, { bool isExpanded = false }) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.white,
-          border: Border.all(
-            color: Colors.white,
-          ),
-          borderRadius:  BorderRadius.only(
-            topLeft: Radius.circular(circularRadius),
-            topRight: Radius.circular(circularRadius),
-          )
+        color: Theme.of(context).backgroundColor,
+        borderRadius: radius,
       ),
-      height: MediaQuery.of(context).size.height * 0.35,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+      margin: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+      height: isExpanded ? 100.0 : 200.0,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'Your Location',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            if (!isExpanded)
+              const Center(
+                child: Icon (Icons.keyboard_arrow_up, size: 40, color: Colors.blue,),
               ),
-            ),
-            const SizedBox(height: 10,),
-            TextField(
-              cursorHeight: 20,
-              autofocus: false,
-              controller: TextEditingController(text: "Our House. In the middle of the street, our house."),
-              decoration: InputDecoration(
-                hintText: "Enter your Name",
-                prefixIcon: const Icon(Icons.location_city),
-                contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(circularRadius),
-                  borderSide: const BorderSide(color: Colors.grey, width: 2),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(circularRadius),
-                  borderSide: const BorderSide(color: Colors.grey, width: 1.5),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  gapPadding: 0.0,
-                  borderRadius: BorderRadius.circular(circularRadius),
-                  borderSide: const BorderSide(color: Colors.red, width: 1.5),
-                ),
+            if (isExpanded)
+              const Center(
+                      child: Icon (Icons.keyboard_arrow_down, size: 40, color: Colors.blue,),
               ),
-            ),
-            const SizedBox(height: 10,),
-            const Text(
-              'Your destination',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10,),
-            TextField(
-              cursorHeight: 20,
-              autofocus: false,
-              controller: TextEditingController(text: "UAB Escola d'Enginyeria"),
-              decoration: InputDecoration(
-                hintText: "Enter your Name",
-                prefixIcon: const Icon(Icons.location_city),
-                contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(circularRadius),
-                  borderSide: const BorderSide(color: Colors.grey, width: 2),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(circularRadius),
-                  borderSide: const BorderSide(color: Colors.grey, width: 1.5),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  gapPadding: 0.0,
-                  borderRadius: BorderRadius.circular(circularRadius),
-                  borderSide: const BorderSide(color: Colors.red, width: 1.5),
+            Padding(
+              padding:  EdgeInsets.only(left: 16.0, right: 16.0, bottom: (!isExpanded) ?  0.0 : 16.0),
+              child: Text(
+                widget.title,
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
           ],
         ),
-      ),
-
     );
   }
+
+  Widget buildBottomSheet(BorderRadiusGeometry radius) {
+
+    FocusNode textSecondFocusNode = new FocusNode();
+    FocusNode sendButtonFocusNode = new FocusNode();
+
+    return Column(
+      children: <Widget>[
+        buildCollapsed(radius, isExpanded: true),
+        Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(circularRadius),
+                bottomRight: Radius.circular(circularRadius),
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 20.0,
+                  color: Colors.grey,
+                ),
+              ]
+          ),
+          padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 24.0),
+          margin: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 24.0),
+          child:
+          Column(
+            children: <Widget>[
+              const SizedBox(height: 10,),
+              const Text(
+                'Your Location',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextFormField(
+                    cursorHeight: 20,
+                    onFieldSubmitted: (String value) {
+                      saveLocation(value, true);
+                      FocusScope.of(context).requestFocus(textSecondFocusNode);
+                    },
+                    //controller: TextEditingController(),
+                    decoration: buildInputDecoration(),
+              ),
+              const SizedBox(height: 10,),
+              const Text(
+                'Your destination',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextFormField(
+                cursorHeight: 20,
+                //controller: TextEditingController(text: "UAB Escola d'Enginyeria"),
+                decoration: buildInputDecoration(),
+                focusNode: textSecondFocusNode,
+                onFieldSubmitted: (String value) {
+                  saveLocation(value, true);
+                  FocusScope.of(context).requestFocus(textSecondFocusNode);
+                },
+              ),
+              const SizedBox(height: 10,),
+              ElevatedButton(
+                onPressed: () {
+                  createPolyLine(polylineCoordinates);
+                  zoomToPolyline();
+                },
+                child: const Text('Leets goo'),
+                focusNode: sendButtonFocusNode,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
+  }
+
+  void saveLocation(String location, bool isDestination) async {
+    if (location.isNotEmpty) {
+      moveToPossibleLatLong(location);
+
+      setState(() {
+        if (isDestination) {
+          _destination = location;
+        } else {
+          _location = location;
+        }
+      });
+    }
+  }
+
+  InputDecoration  buildInputDecoration(){
+    return InputDecoration(
+      hintText: "Enter your Name",
+      prefixIcon: const Icon(Icons.location_city),
+      contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(circularRadius),
+        borderSide: const BorderSide(color: Colors.grey, width: 2),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(circularRadius),
+        borderSide: const BorderSide(color: Colors.grey, width: 1.5),
+      ),
+      focusedBorder: OutlineInputBorder(
+        gapPadding: 0.0,
+        borderRadius: BorderRadius.circular(circularRadius),
+        borderSide: const BorderSide(color: Colors.red, width: 1.5),
+      ),
+    );
+  }
+
 }
