d5ba107cf6ac3b92362f16d26fa31e9d11da9fbb
marti1999 <mcaixal1999@gmail.com>
2022-05-04 19:48:08 +0200

    creating firebase database connection

--- rfid_hackaton/lib/services/database.dart
+++ rfid_hackaton/lib/services/database.dart
@@ -2,7 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
 
+  final String userID;
+  DatabaseService({required this.userID});
   // collection reference
   final CollectionReference userCol = FirebaseFirestore.instance.collection('users');
+
+  Future updateUserData(String name, int age) async {
+    return await userCol.doc(userID).set({
+      'name':name,
+      'age':age,
+    });
+  }
 
 }
