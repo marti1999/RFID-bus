a00f473f473c205dfdd6db6c8b646fc809805a9a
marti1999 <mcaixal1999@gmail.com>
2022-05-04 20:54:52 +0200

    Firebase initialise app

--- rfid_hackaton/lib/main.dart
+++ rfid_hackaton/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rfid_hackaton/views/map_view.dart';
 import 'package:rfid_hackaton/views/profile_view.dart';
 import 'package:rfid_hackaton/services/database.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
