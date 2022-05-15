ad4e3c4989d2ca53c4cd1d5bd7f55e17bac7a03a
marti1999 <mcaixal1999@gmail.com>
2022-05-15 13:44:49 +0200

    dowload profile picture from Storage API

--- rfid_hackaton/lib/views/profile/widgets/profile_widget.dart
+++ rfid_hackaton/lib/views/profile/widgets/profile_widget.dart
@@ -4,6 +4,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileWidget extends StatefulWidget {
   const ProfileWidget({Key? key, required this.onClicked}) : super(key: key);
@@ -13,36 +14,43 @@ class ProfileWidget extends StatefulWidget {
   State<ProfileWidget> createState() => _ProfileWidgetState();
 }
 
-class _ProfileWidgetState extends State<ProfileWidget> {
-  late String imageUrl;
+String _userid = '';
+String imageUrl = '';
+late Image profilePic;
 
+class _ProfileWidgetState extends State<ProfileWidget> {
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).colorScheme.primary;
 
-    return Center(
-      child: Stack(
-        children: [
-          buildImage(),
-          Positioned(
-            bottom: 0,
-            right: 4,
-            child: buildEditIcon(color),
-          ),
-        ],
-      ),
-    );
+    return FutureBuilder(
+        future: getImageURL(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Center(
+              child: Stack(
+                children: [
+                  buildImage(),
+                  Positioned(
+                    bottom: 0,
+                    right: 4,
+                    child: buildEditIcon(color),
+                  ),
+                ],
+              ),
+            );
+          }else return CircularProgressIndicator();
+        });
   }
 
   Widget buildImage() {
     //TODO change this and get it from storage
-    final image = NetworkImage('https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png');
 
     return ClipOval(
       child: Material(
         color: Colors.transparent,
         child: Ink.image(
-          image: image,
+          image: profilePic.image,
           fit: BoxFit.cover,
           width: 128,
           height: 128,
@@ -62,7 +70,6 @@ class _ProfileWidgetState extends State<ProfileWidget> {
             Icons.add_a_photo,
             color: Colors.white,
             size: 20,
-
           ),
         ),
       );
@@ -79,4 +86,23 @@ class _ProfileWidgetState extends State<ProfileWidget> {
           child: child,
         ),
       );
+}
+
+Future<String> getImageURL() async {
+  // final gsReference = FirebaseStorage.instance.refFromURL("gs://graphical-bus-348706.appspot.com/YfY0jrDouVVCfkMCl21aOSJvjpf2.jpg");
+  // imageUrl = await gsReference.getDownloadURL();
+  // final Image image = Image.network(await gsReference.getDownloadURL());
+  // return image;
+
+  final prefs = await SharedPreferences.getInstance();
+  _userid = prefs.getString('uid') ?? '';
+
+  final ref = FirebaseStorage.instance.ref().child(_userid + '.jpg');
+  String a = await ref.getDownloadURL();
+  imageUrl = a;
+
+  profilePic = Image.network(a.toString());
+
+  print('puta merda fluter');
+  return a;
 }
