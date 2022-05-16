d7e426245470664f603784688b9386a28816470a
Bruno Moya <elblogdebruno@outlook.es>
2022-05-16 10:44:43 +0200

    feat: fix bugs in simulation

--- python/random_buses_feed.py
+++ python/random_buses_feed.py
@@ -12,10 +12,10 @@ from random import uniform
 import requests
 from bs4 import BeautifulSoup
 import copy
-import json
-import os
 
 from decimal import *
+
+from pyproj import Geod
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('secret-key.json')
@@ -64,7 +64,8 @@ class BusLine:
         self.next_stop = stops[1]
         
         self.stops = stops
-
+        self.geoid = Geod(ellps='WGS84')
+    
         getcontext().prec = 7
         
         self.lat = Decimal(self.stop.lat)
@@ -93,122 +94,91 @@ class BusLine:
             'people_number': self.people_number
         }
 
-    def move_to_next_stop(self, speed = 1):
-        
-        increment_lat = Decimal((self.next_stop.lat - self.stop.lat) / speed)
-        increment_lng = Decimal((self.next_stop.lng - self.stop.lng) / speed)
 
-        if self.stop.lat > self.next_stop.lat: 
-            print("stop lat is smaller than next stop lat")
-            increment_lat = -increment_lat
+    def add_distance(self, lat, lng, az, dist):
+        lng_new, lat_new, return_az = self.geoid.fwd(lng, lat, az, dist)
+        return lat_new, lng_new
 
-        if self.stop.lng > self.next_stop.lng:
-            print("stop lng is smaller than next stop lng")
-            increment_lng = -increment_lng
-
-        print(self.lat, self.lng, self.next_stop.lat, self.next_stop.lng)
-
-        # Update the bus position in the database meter to meter until current lat and long are the same as the next stop lat and long
-        while self.lat != self.next_stop.lat and self.lng != self.next_stop.lng:
-            self.lat += increment_lat 
-            self.lng += increment_lng
-
+    def move_to_next_stop(self, speed = 10):
+    
+        # Given a single initial point and terminus point, and the number of points, returns a list of longitude/latitude pairs describing npts equally spaced intermediate points along the geodesic between the initial and terminus points.
+        r = self.geoid.inv_intermediate(self.lng,self.lat,self.next_stop.lng, self.next_stop.lat, speed)
+        for lon,lat in zip(r.lons, r.lats): 
+            self.lat = lat
+            self.lng = lon
             self.update_bus()
-            sleep(0.1)
-
-            print(f"{self.line_id} {self.line_name} {self.stop} {self.lat} {self.lng}")
+            sleep(random.uniform(0.1, 0.5))
 
 
         print("Reached next stop")
 
         # Update the bus position in the database to the next stop
-        self.people_number -= random.randint(1, 10) # Random number between 1 and 10 people leave the bus
-        self.people_number += random.randint(1, 10) # Random number between 1 and 10 people entering the bus
-
+        self.people_number -= random.randint(1, 10) # Random number between 1 and 10 people left
+        self.people_number += random.randint(1, 10) # Random number between 1 and 10 people entering
+        
+        self.people_number = max(self.people_number, 0) # Make sure people number is not negative
+        
         self.stop = self.next_stop # Update the current stop
 
-        self.next_stop = self.stops[self.current_stop_index + 1] # Update the next stop
-        self.current_stop_index += 1 # Update the current stop index
+        # Update the current stop index
+        if self.current_stop_index + 1 < len(self.stops):
+            self.current_stop_index += 1
+        else:
+            self.current_stop_index = 0
+
+        self.next_stop = self.stops[self.current_stop_index] # Update the next stop
         self.update_bus() # Update the bus position in the database
 
     def update_bus(self):
-        # doc_ref.document(value.line_id).set(value.to_json())
-        ref.child(self.line_id).set(self.to_json())
-        # ref.child(self.line_id).set(
-        #     {
-        #         'busId': self.line_id,
-        #         'busLine': self.line_name, 
-        #         'busPeopleNumber': self.people_number, 
-        #         'busLatitude': float(self.lat), 
-        #         'busLongitude': float(self.lng),
-        #         'busStop': self.stop.name,
-        #         'busNextStop': self.next_stop.name,
-        #         'busNextStopLatitude': self.next_stop.lat,
-        #         'busNextStopLongitude': self.next_stop.lng
+        ref.child(self.line_id).set(
+            {
+                'busId': self.line_id,
+                'busLine': self.line_name, 
+                'busPeopleNumber': self.people_number, 
+                'busLatitude': float(self.lat), 
+                'busLongitude': float(self.lng),
+                'busStop': self.stop.name,
+                'busNextStop': self.next_stop.name,
+                'busNextStopLatitude': self.next_stop.lat,
+                'busNextStopLongitude': self.next_stop.lng
                 
-        #     })
+            })
 
 
 UAB_STOPS_ENDPOINT = "http://appbuses.accessibilitat-transports.uab.cat/?mod=linesviewer&page=lines_viewer_getmapdata&json=1"
 UAB_LINE_STOPS_ENDPOINT = "http://appbuses.accessibilitat-transports.uab.cat/?mod=linesviewer&page=lines_viewer_busstop&ajax=1"
 
 def read_bus_data():
-    if os.path.exists("uab_bus_data.json"):
-        print("Loading UAB bus data from file...")
-        with open("uab_bus_data.json", "r") as f:
-            data = json.load(f)
-            return data['parades'], data['linies']
-    else:
-        print("Fetching UAB bus data...")
-        # make a request to the endpoint
-        response = requests.get(UAB_STOPS_ENDPOINT)
-        # parse the response
-        data = response.json()
-        print(data)
-
-        # save to json file 
-        with open('uab_bus_data.json', 'w') as outfile:
-            json.dump(data, outfile)
-
-        # return the data
-        return data['parades'], data['linies']
+    # make a request to the endpoint
+    response = requests.get(UAB_STOPS_ENDPOINT)
+    # parse the response
+    data = response.json()
+    # return the data
+    return data['parades'], data['linies']
 
 def get_lines_stop(data):
     lines = {}
 
-    if os.path.exists("uab_bus_lines.json"):
-        print("Loading UAB bus lines from file...")
-        with open("uab_bus_lines.json", "r") as f:
-            lines = json.load(f)
-            return lines
-    
-    else:
-        print("Fetching UAB bus lines...")
+    for linia in data:
+        # make post request to UAB_LINE_STOPS_ENDPOINT with the line id
+        response = requests.post(UAB_LINE_STOPS_ENDPOINT, data={'IdLinia': linia['IdLinia']})
+        # parse the html response
+        html = response.text
+        # parse the html response
+        soup = BeautifulSoup(html, 'html.parser')
+        # get all divs with atribute 'idparada' and get the text
+        stops = soup.find_all('div', {'idparada': True})
+        
+        lines[linia['IdLinia']] = []
 
-        for linia in data:
-            # make post request to UAB_LINE_STOPS_ENDPOINT with the line id
-            response = requests.post(UAB_LINE_STOPS_ENDPOINT, data={'IdLinia': linia['IdLinia']})
-            # parse the html response
-            html = response.text
-            # parse the html response
-            soup = BeautifulSoup(html, 'html.parser')
-            # get all divs with atribute 'idparada' and get the text
-            stops = soup.find_all('div', {'idparada': True})
+        # for each stop get the 'idparada' value
+        for stop in stops:
+            # get the idparada value
+            idparada = stop['idparada']
             
-            lines[linia['IdLinia']] = []
+            lines[linia['IdLinia']].append(idparada)
 
-            # for each stop get the 'idparada' value
-            for stop in stops:
-                # get the idparada value
-                idparada = stop['idparada']
-                
-                lines[linia['IdLinia']].append(idparada)
-
-        # save to json file
-        with open('uab_bus_lines.json', 'w') as outfile:
-            json.dump(lines, outfile)
-
-        return lines
+    return lines
 
 def get_stops(data):
     stops = []
@@ -256,7 +226,7 @@ buses = initialize_buses(stops, bus_lines, lines_with_stops)
 
 while True:
     for bus in buses:
-        bus.move_to_next_stop(speed = 1)
+        bus.move_to_next_stop(speed = 10)
         sleep(1)
         print("Current BUS: " + str(bus))
         # ref.child('bus{0}'.format(i)).set({'busId': str(i), 'busPeopleNumber': random.randint(1, 200), 'busTime': '12:00', 'busLatitude': stop['latitude'], 'busLongitude': stop['longitude']})
