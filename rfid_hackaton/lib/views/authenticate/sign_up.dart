eab0f4972beff66f864035e5414c0ff12f24510a
Ricardlol <ricardlopezolivares@gmail.com>
2022-05-21 21:43:49 +0200

    scroll

--- rfid_hackaton/lib/views/authenticate/sign_up.dart
+++ rfid_hackaton/lib/views/authenticate/sign_up.dart
@@ -61,7 +61,7 @@ class _RegisterState extends State<Register> {
         await storage.ref(fileName).putFile(
             imageFile,
             SettableMetadata(customMetadata: {
-              'uploaded_by': 'martí',
+              'uploaded_by': 'NFCID',
               'description': 'Some description...'
             }));
 
@@ -79,19 +79,6 @@ class _RegisterState extends State<Register> {
     }
   }
 
-  // Future pickImageGallery() async{
-  //   try{
-  //     final image = await ImagePicker().pickImage(source : ImageSource.gallery);
-  //     if(image == null) return ;
-  //     final imageTemporary = File(image.path);
-  //     setState(() => this.imageFile = imageTemporary );
-  //   } on PlatformException catch(e){
-  //     print('Failed to pick image: $e');
-  //   }
-  // }
-  //  Future pickImageCamera() async{
-  //    await ImagePicker().pickImage(source : ImageSource.camera);
-  //  }
   @override
   Widget build(BuildContext context) {
     return loading
@@ -111,179 +98,184 @@ class _RegisterState extends State<Register> {
                     })
               ],
             ),
-            body: Container(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-                child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextFormField(
-                          // validator: (val) => val!.isEmpty ? 'Enter an Email': null,
+            body: SingleChildScrollView(
+              child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                  child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            // validator: (val) => val!.isEmpty ? 'Enter an Email': null,
 
-                          decoration:
-                              textInputDecoration.copyWith(hintText: 'Name'),
-                          validator: (val) {
-                            if (val != null && val.isEmpty) {
-                              return "Enter an Name";
-                            } else {
-                              return null;
-                            }
-                          },
-                          onChanged: (val) {
-                            setState(() => name = val);
-                          },
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextFormField(
-                          // validator: (val) => val!.isEmpty ? 'Enter an Email': null,
-
-                          decoration:
-                              textInputDecoration.copyWith(hintText: 'Email'),
-                          validator: (val) {
-                            if (val != null && val.isEmpty) {
-                              return "Enter an Email";
-                            } else {
-                              return null;
-                            }
-                          },
-                          onChanged: (val) {
-                            setState(() => email = val);
-                          },
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: textInputDecoration.copyWith(
-                              hintText: 'Password'),
-                          validator: (val) {
-                            if (val!.length < 6) {
-                              return 'Enter an password 6+ chars long';
-                            } else {
-                              return null;
-                            }
-                          },
-                          onChanged: (val) {
-                            setState(() => passwd = val);
-                          },
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        DropdownButtonFormField(
-                          value: city,
-                          icon: Icon(Icons.arrow_downward),
-                          onChanged: (value) {
-                            setState(() {
-                              city = value.toString();
-                            });
-                          },
-                          items: [
-                            DropdownMenuItem(
-                                child: Text("Barcelona"), value: "Barcelona"),
-                            DropdownMenuItem(
-                                child: Text("Sabadell"), value: "Sabadell"),
-                            DropdownMenuItem(
-                                child: Text("San Cugat"), value: "San Cugat"),
-                            DropdownMenuItem(
-                                child: Text("Terrassa"), value: "Terrassa")
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        ListTile(
-                          title: Text("Male"),
-                          leading: Radio(
-                            value: "Male",
-                            groupValue: sex,
-                            onChanged: (value) {
-                              setState(() {
-                                sex = value.toString();
-                              });
-                            },
-                            activeColor: Colors.blue,
-                          ),
-                        ),
-                        ListTile(
-                          title: Text("Female"),
-                          leading: Radio(
-                            value: "Female",
-                            groupValue: sex,
-                            onChanged: (value) {
-                              setState(() {
-                                sex = value.toString();
-                              });
-                            },
-                            activeColor: Colors.blue,
-                          ),
-                        ),
-                        ListTile(
-                          title: Text("Other"),
-                          leading: Radio(
-                            value: "Other",
-                            groupValue: sex,
-                            onChanged: (value) {
-                              setState(() {
-                                sex = value.toString();
-                              });
-                            },
-                            activeColor: Colors.blue,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        ElevatedButton.icon(
-                            icon: Icon(Icons.image_outlined),
-                            label: Text("Pick Gallery"),
-                            onPressed: () => _pickImage()),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.pink[400],
-                            ),
-                            child: const Text('Sign up'),
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                setState(() => loading = true);
-                                dynamic result =
-                                    await _auth.registerWithEmailAndPassword(
-                                        email,
-                                        passwd,
-                                        name,
-                                        imagePath,
-                                        sex,
-                                        city);
-                                if (result == null) {
-                                  setState(() {
-                                    error =
-                                        'could not sign in with credentials';
-                                    loading = false;
-                                  });
-                                } else{
-                                  await _uploadWithUser();
-                                  _upload(_userid);
-                                }
+                            decoration:
+                                textInputDecoration.copyWith(hintText: 'Name'),
+                            validator: (val) {
+                              if (val != null && val.isEmpty) {
+                                return "Enter an Name";
+                              } else {
+                                return null;
                               }
-                            }),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Text(
-                          error,
-                          style: TextStyle(color: Colors.red, fontSize: 14.0),
-                        ),
-                      ],
-                    ))));
+                            },
+                            onChanged: (val) {
+                              setState(() => name = val);
+                            },
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            // validator: (val) => val!.isEmpty ? 'Enter an Email': null,
+
+                            decoration:
+                                textInputDecoration.copyWith(hintText: 'Email'),
+                            validator: (val) {
+                              if (val != null && val.isEmpty) {
+                                return "Enter an Email";
+                              } else {
+                                return null;
+                              }
+                            },
+                            onChanged: (val) {
+                              setState(() => email = val);
+                            },
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Password'),
+                            validator: (val) {
+                              if (val!.length < 6) {
+                                return 'Enter an password 6+ chars long';
+                              } else {
+                                return null;
+                              }
+                            },
+                            onChanged: (val) {
+                              setState(() => passwd = val);
+                            },
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          DropdownButtonFormField(
+                            value: city,
+                            icon: Icon(Icons.arrow_downward),
+                            onChanged: (value) {
+                              setState(() {
+                                city = value.toString();
+                              });
+                            },
+                            items: [
+                              DropdownMenuItem(
+                                  child: Text("Barcelona"), value: "Barcelona"),
+                              DropdownMenuItem(
+                                  child: Text("Sabadell"), value: "Sabadell"),
+                              DropdownMenuItem(
+                                  child: Text("San Cugat"), value: "San Cugat"),
+                              DropdownMenuItem(
+                                  child: Text("Terrassa"), value: "Terrassa")
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          ListTile(
+                            title: Text("Male"),
+                            leading: Radio(
+                              value: "Male",
+                              groupValue: sex,
+                              onChanged: (value) {
+                                setState(() {
+                                  sex = value.toString();
+                                });
+                              },
+                              activeColor: Colors.blue,
+                            ),
+                          ),
+                          ListTile(
+                            title: Text("Female"),
+                            leading: Radio(
+                              value: "Female",
+                              groupValue: sex,
+                              onChanged: (value) {
+                                setState(() {
+                                  sex = value.toString();
+                                });
+                              },
+                              activeColor: Colors.blue,
+                            ),
+                          ),
+                          ListTile(
+                            title: Text("Other"),
+                            leading: Radio(
+                              value: "Other",
+                              groupValue: sex,
+                              onChanged: (value) {
+                                setState(() {
+                                  sex = value.toString();
+                                });
+                              },
+                              activeColor: Colors.blue,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          ElevatedButton.icon(
+                              icon: Icon(Icons.image_outlined),
+                              label: Text("Pick Gallery"),
+                              onPressed: () => _pickImage()),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.pink[400],
+                              ),
+                              child: const Text('Sign up'),
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  setState(() => loading = true);
+                                  dynamic result =
+                                      await _auth.registerWithEmailAndPassword(
+                                          email,
+                                          passwd,
+                                          name,
+                                          imagePath,
+                                          sex,
+                                          city);
+                                  if (result == null) {
+                                    setState(() {
+                                      error =
+                                          'could not sign in with credentials';
+                                      loading = false;
+                                    });
+                                  } else{
+                                    await _uploadWithUser();
+                                    _upload(_userid);
+                                  }
+                                }
+                              }),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            error,
+                            style: TextStyle(color: Colors.red, fontSize: 14.0),
+                          ),
+                        ],
+                      )))
+        ],
+    )));
   }
 
   Future<File> saveFilePermanently(PlatformFile file) async {
