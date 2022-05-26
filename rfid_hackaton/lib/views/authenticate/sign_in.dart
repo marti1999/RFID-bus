f4e4c9f3c7acd75716c5286e4faf221b3c950d34
Ricardlol <ricardlopezolivares@gmail.com>
2022-05-26 12:24:23 +0200

    scroll bar sign in

--- rfid_hackaton/lib/views/authenticate/sign_in.dart
+++ rfid_hackaton/lib/views/authenticate/sign_in.dart
@@ -44,81 +44,85 @@ class _SignInState extends State<SignIn> {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child:Column(
-            children: <Widget>[
-              SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) {
-                  if(val != null && val.isEmpty ){
-                    return "Enter an Email";
-                  }else{
-                    return null;
-                  }
-                },
-                onChanged: (val){
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0,),
-              TextFormField(
-                obscureText: true,
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                validator: (val) {
-                  if (val!.length < 6) {
-                    return 'Enter an password 6+ chars long';
-                  }
-                  else {
-                    return null;
-                  }
+      body: SingleChildScrollView(
+          child: Stack(
+              children: <Widget>[
+                  Container(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                  child: Form(
+                    key: _formKey,
+                    child:Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0,),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                          validator: (val) {
+                            if(val != null && val.isEmpty ){
+                              return "Enter an Email";
+                            }else{
+                              return null;
+                            }
+                          },
+                          onChanged: (val){
+                            setState(() => email = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0,),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                          validator: (val) {
+                            if (val!.length < 6) {
+                              return 'Enter an password 6+ chars long';
+                            }
+                            else {
+                              return null;
+                            }
 
-                },
-                onChanged: (val){
-                  setState(() => passwd = val);
-                },
-              ),
-              SizedBox(height: 20.0,),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.pink[400],
-                ),
-                child: const Text('Sign in'),
-                onPressed: () async {
-                  if(_formKey.currentState!.validate()){
-                    setState(() => loading = true);
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, passwd);
-                    if(result == null){
-                      setState(() {
-                        error = 'could not sign in with credentials';
-                        loading = false;
-                      });
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 20.0,),
-              Text(
-                error,
-                style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 14.0
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                child: Text('Realtime (IoT per buseros)'),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const RealtimeDashboard(title: 'IoT Bus Company',)));
-                },
-              ),
-            ],
-          )
-        )
-      )
+                          },
+                          onChanged: (val){
+                            setState(() => passwd = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0,),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.pink[400],
+                          ),
+                          child: const Text('Sign in'),
+                          onPressed: () async {
+                            if(_formKey.currentState!.validate()){
+                              setState(() => loading = true);
+                              dynamic result = await _auth.signInWithEmailAndPassword(email, passwd);
+                              if(result == null){
+                                setState(() {
+                                  error = 'could not sign in with credentials';
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                        ),
+                        SizedBox(height: 20.0,),
+                        Text(
+                          error,
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 14.0
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          child: Text('Realtime (IoT per buseros)'),
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const RealtimeDashboard(title: 'IoT Bus Company',)));
+                          },
+                        ),
+                      ],
+                    )
+                  )
+                )
+      ]))
     );
   }
 }
